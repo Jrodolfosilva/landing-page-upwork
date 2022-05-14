143ca5f0352626f64ce7739268b63e1683136a91
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-05-13 22:00:33 -0300

    Update css
    Correção de bug no menu mobile > box-social

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const main = document.querySelector("main");
+const boxSocial = document.querySelector(".box-social-mobile")
 const menuMobile = document.querySelector(".menu-mobile");
 const boxMenu = document.querySelector(".box-menu")
 const itemMenu = document.querySelectorAll(".item-menu");
@@ -7,6 +8,7 @@ function showMenu(){
     
     boxMenu.classList.toggle('ativo')
     main.classList.toggle('ativo')
+    boxSocial.classList.toggle("ativo")
 }
 
 
