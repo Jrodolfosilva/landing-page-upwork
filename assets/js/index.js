4da8f70b764fc32ef48bc94056b9f6995c425dee
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-05-13 23:08:48 -0300

    Update css js
    Ajusto no menu mobile;
    organização do código JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,29 @@
-const main = document.querySelector("main");
-const boxSocial = document.querySelector(".box-social-mobile")
-const menuMobile = document.querySelector(".menu-mobile");
-const boxMenu = document.querySelector(".box-menu")
-const itemMenu = document.querySelectorAll(".item-menu");
-const barras =document.querySelectorAll(".br")
+function interacaoMenu(){
+
+        const main = document.querySelector("main");
+        const boxSocial = document.querySelector(".box-social-mobile")
+        const menuMobile = document.querySelector(".menu-mobile");
+        const boxMenu = document.querySelector(".box-menu")
+        const itemMenu = document.querySelectorAll(".item-menu");
+        const barras =document.querySelectorAll(".br")
 
 
-function showMenu(){
-    
-    boxMenu.classList.toggle('ativo')
-    main.classList.toggle('ativo')
-    boxSocial.classList.toggle("ativo")
-    barras.forEach((i)=>{
-        i.classList.toggle("ativo")
-    })
+        function showMenu(){
+            
+            boxMenu.classList.toggle('ativo')
+            main.classList.toggle('desativado')
+            boxSocial.classList.toggle("ativo")
+            barras.forEach((i)=>{
+                i.classList.toggle("ativo")
+            })
+        }
+
+        menuMobile.addEventListener('click',showMenu
+        )
+
+        itemMenu.forEach((item)=>{
+
+            item.addEventListener('click',showMenu)
+        })
 }
-
-
-
-menuMobile.addEventListener('click',showMenu
-)
-
-itemMenu.forEach((item)=>{
-
-    item.addEventListener('click',showMenu)
-})
+interacaoMenu()
